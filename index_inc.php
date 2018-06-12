38dc7123218bc677b2800b723491dd4065c287f7
adoletskiy <adoletsky@indigos.ru>
2018-06-12 20:59:51 +0300

    header

---
+++ index_inc.php
@@ -0,0 +1,14 @@
+<div class="dd-gallery__list row">
+    <a class="dd-gallery__item _oboi col-12 col-sm-3 col-lg-3">
+        <div class="dd-gallery__item-title">Обои</div>
+    </a>
+    <a class="dd-gallery__item _kraski col-12 col-sm-3 col-lg-3">
+        <div class="dd-gallery__item-title">Краски</div>
+    </a>
+    <a class="dd-gallery__item _lepnina col-12 col-sm-3 col-lg-3">
+        <div class="dd-gallery__item-title">Лепнина</div>
+    </a>
+    <a class="dd-gallery__item _textil col-12 col-sm-3 col-lg-3">
+        <div class="dd-gallery__item-title">Текстиль</div>
+    </a>
+</div>
--- local/templates/new-decor-delux/header.php
+++ local/templates/new-decor-delux/header.php
@@ -54,19 +54,11 @@
 </header>
 
 <section class="dd-gallery container-fluid">
-    <div class="dd-gallery__list row">
-        <a class="dd-gallery__item _oboi col-12 col-sm-3 col-lg-3">
-            <div class="dd-gallery__item-title">Обои</div>
-        </a>
-        <a class="dd-gallery__item _kraski col-12 col-sm-3 col-lg-3">
-            <div class="dd-gallery__item-title">Краски</div>
-        </a>
-        <a class="dd-gallery__item _lepnina col-12 col-sm-3 col-lg-3">
-            <div class="dd-gallery__item-title">Лепнина</div>
-        </a>
-        <a class="dd-gallery__item _textil col-12 col-sm-3 col-lg-3">
-            <div class="dd-gallery__item-title">Текстиль</div>
-        </a>
-    </div>
+    <?$APPLICATION->IncludeComponent("bitrix:main.include","",Array(
+            "AREA_FILE_SHOW" => "page",
+            "AREA_FILE_SUFFIX" => "inc",
+            "EDIT_TEMPLATE" => ""
+        )
+    );?>
 </section>
 
